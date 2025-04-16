2d401d4d35da86da0897a9399aa9d6b3c53d71af
AmandaCornelsen <cornelsen089@gmail.com>
2025-04-16 19:43:25 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { InterpolacaoComponent } from './components/interpolacao/interpolacao.component';
+import { ControlFlowComponent } from './components/control-flow/control-flow.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, InterpolacaoComponent, ControlFlowComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'app-control-flow';
+}
